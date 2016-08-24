712ba8aa5f4ced6d04520273c01461faee85f14e
Stefan Kauerauf <mail@stefankauerauf.de>
2016-08-24 16:19:37 +0200

    fixed comment

--- check_smart.py
+++ check_smart.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python2
 
-/**
- * check_smart.py
- * 
- * Copyright (C) 2015 Stefan Kauerauf <mail@stefankauerauf.de>
- * 
- * This file is part of check_smart.
- * 
- * check_smart is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * check_smart is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * See the GNU General Public License for more details. You should have received a copy of the GNU
- * General Public License along with check_smart. If not, see <http://www.gnu.org/licenses/>.
- * 
- * Authors: Stefan Kauerauf
- */
+ # check_smart.py
+ # 
+ # Copyright (C) 2015 Stefan Kauerauf <mail@stefankauerauf.de>
+ # 
+ # This file is part of check_smart.
+ # 
+ # check_smart is free software: you can redistribute it and/or modify it under the terms of the
+ # GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ # License, or (at your option) any later version.
+ # 
+ # check_smart is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ # 
+ # See the GNU General Public License for more details. You should have received a copy of the GNU
+ # General Public License along with check_smart. If not, see <http://www.gnu.org/licenses/>.
+ # 
+ # Authors: Stefan Kauerauf
 
 import argparse
 from pySMART import Device
